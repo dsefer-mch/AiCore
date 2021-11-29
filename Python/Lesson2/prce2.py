9d21128e30117730053839cbaa4a9c18e495cd04
mch <mch.dv.uk@gmai.com>
2021-11-29 22:31:59 +0000

    Edits the second practice file

--- Python/Lesson2/prce2.py
+++ Python/Lesson2/prce2.py
@@ -10,9 +10,13 @@
 
 a = 42
 b = 25
+ 
 print(a + b)
 print(a - b)
 print(a * b)
 print(a / b)
 a = 4.2
-print(type(a + b))
+print(type(a + b))   #in Python the result inheritate
+                     #the type <float> over <int>
+                     #when the operators are mixed 
+                
